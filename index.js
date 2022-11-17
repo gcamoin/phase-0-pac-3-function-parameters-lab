db442d9d80629b58ba3180411119b72160b891c4
Garrett <gcamoin5@gmail.com>
2022-11-17 18:23:17 -0500

    commit tree 9ed8d38b332d956392bf7431944617c65b7e289f

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(firstname) {
+    return(`Hi, my name is ${firstname}.`);
+}
+function introductionWithLanguage(name = "Aki", language = "Ember.js") {
+return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+    }
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
